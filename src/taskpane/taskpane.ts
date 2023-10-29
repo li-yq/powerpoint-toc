9aedb13b30913b69c9be697e50499f91bec0372f
Li Yuanqing <39945542+li-yq@users.noreply.github.com>
2023-10-29 11:12:58 +0800

    testing image insertion

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -5,20 +5,55 @@
 
 /* global document, Office */
 
+import { base64Image } from "../base64Image";
+
 Office.onReady((info) => {
   if (info.host === Office.HostType.PowerPoint) {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
-    document.getElementById("run").onclick = run;
+    document.getElementById("insert-image").onclick = () => clearMessage(insertImage);
+    // TODO4: Assign event handler for insert-text button.
+    // TODO6: Assign event handler for get-slide-metadata button.
+    // TODO8: Assign event handlers for add-slides and the four navigation buttons.
   }
 });
 
-export async function run() {
-  /**
-   * Insert your PowerPoint code here
-   */
-  const options: Office.SetSelectedDataOptions = { coercionType: Office.CoercionType.Text };
+function insertImage() {
+  // Call Office.js to insert the image into the document.
+  Office.context.document.setSelectedDataAsync(
+    base64Image,
+    {
+      coercionType: Office.CoercionType.Image
+    },
+    (asyncResult) => {
+      if (asyncResult.status === Office.AsyncResultStatus.Failed) {
+        setMessage("Error: " + asyncResult.error.message);
+      }
+    }
+  );
+}
 
-  await Office.context.document.setSelectedDataAsync(" ", options);
-  await Office.context.document.setSelectedDataAsync("Hello World!", options);
+// TODO5: Define the insertText function.
+
+// TODO7: Define the getSlideMetadata function.
+
+// TODO9: Define the addSlides and navigation functions.
+
+async function clearMessage(callback) {
+  document.getElementById("message").innerText = "";
+  await callback();
+}
+
+function setMessage(message) {
+  document.getElementById("message").innerText = message;
+}
+
+// Default helper for invoking an action and handling errors.
+async function tryCatch(callback) {
+  try {
+    document.getElementById("message").innerText = "";
+    await callback();
+  } catch (error) {
+    setMessage("Error: " + error.toString());
+  }
 }
